f9fb2f44697f4a89ac99f67e26c381959e95df1b
luiszkm <luiszkm@gmail.com>
2023-09-27 20:42:26 -0300

    feat: add Update Category useCase and tests

---
+++ tests/CodeFlix.Catalog.UnitTest/Application/Category/UpdateCategory/UpdateCategoryDataGenerator.cs
@@ -0,0 +1,20 @@
+﻿
+namespace CodeFlix.Catalog.UnitTest.Application.Category.UpdateCategory;
+using UseCase = CodeFlix.Catalog.Application.UseCases.Category.UpdateCategory;
+public class UpdateCategoryDataGenerator
+{
+    public static IEnumerable<object[]> GetCategoriesToUpdate(int times = 10)
+    {
+        var fixture = new UpdateCategoryTestFixture();
+        for (int i = 0; i < times; i++)
+        {
+            var exampleCategory = fixture.GetValidCategory();
+
+            var input = fixture.GetValidInput(exampleCategory.Id);
+
+            yield return new object[] { exampleCategory, input };
+        }
+    }
+
+
+}
--- tests/CodeFlix.Catalog.UnitTest/Application/Category/UpdateCategory/UpdateCategoryTestFixture.cs
+++ tests/CodeFlix.Catalog.UnitTest/Application/Category/UpdateCategory/UpdateCategoryTestFixture.cs
@@ -1,6 +1,67 @@
-﻿
+﻿using CodeFlix.Catalog.Application.Interfaces;
+using CodeFlix.Catalog.Application.UseCases.Category.UpdateCategory;
+using CodeFlix.Catalog.Domain.Domain.Repository;
+using CodeFlix.Catalog.UnitTest.Common;
+using Moq;
 
 namespace CodeFlix.Catalog.UnitTest.Application.Category.UpdateCategory;
-public class UpdateCategoryTestFixture
+
+[CollectionDefinition(nameof(UpdateCategoryTestFixture))]
+public class UpdateCategoryTestFixtureCollection :
+    ICollectionFixture<UpdateCategoryTestFixture>
+{ }
+
+public class UpdateCategoryTestFixture : BaseFixture
 {
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+        while (categoryName.Length < 3)
+        {
+            categoryName = Faker.Commerce.Categories(1)[0];
+        }
+        if (categoryName.Length > 255)
+        {
+            categoryName = categoryName[..255];
+        }
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+        if (categoryDescription.Length > 10_000)
+        {
+            categoryDescription = categoryDescription[..10_000];
+        }
+
+        return categoryDescription;
+    }
+
+    public bool GetRandomBoolean()
+    {
+        return Faker.Random.Bool();
+    }
+
+
+    public DomainEntity.Category GetValidCategory()
+        => new(
+            GetValidCategoryName(),
+            GetValidCategoryDescription(),
+            GetRandomBoolean()
+        );
+
+    public UpdateCategoryInput GetValidInput(Guid? id = null)
+         => new(
+                id ?? Guid.NewGuid(),
+                 GetValidCategoryName(),
+                 GetValidCategoryDescription(),
+                  GetRandomBoolean()
+                );
+
+    public Mock<ICategoryRepository> GetCategoryRepositoryMock()
+      => new();
+
+    public Mock<IUnitOfWork> GetUnitOfWorkMock()
+         => new();
 }
